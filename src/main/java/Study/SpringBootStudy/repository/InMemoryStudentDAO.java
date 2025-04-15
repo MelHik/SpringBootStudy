c1c3a5679916bb1bca9468d4151cdfa0cdbfd250
Ilya Melnikov <ilyamelhik@yandex.ru>
2025-04-16 00:50:37 +0500

    add new dependencies for database use and add missing repository script

---
+++ src/main/java/Study/SpringBootStudy/repository/InMemoryStudentDAO.java
@@ -0,0 +1,44 @@
+package Study.SpringBootStudy.repository;
+
+import Study.SpringBootStudy.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class InMemoryStudentDAO {
+    private final List<Student> STUDENTS = new ArrayList<>();
+
+    public List<Student> findAllStudent(){
+        return STUDENTS;
+    }
+
+    public Student saveStudent(Student student) {
+        STUDENTS.add(student);
+        return student;
+    }
+    public Student findByEmail(String email) {
+        return STUDENTS.stream()
+                .filter(element -> element.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
+    }
+    public Student update(Student student) {
+        var studentIndex = IntStream.range(0, STUDENTS.size())
+                .filter(index -> STUDENTS.get(index).getEmail().equals(student))
+                .findFirst()
+                .orElse(-1);
+        if (studentIndex > -1) {
+            STUDENTS.set(studentIndex, student);
+            return student;
+        }
+        return null;
+    }
+
+    public void deleteStudent(String email) {
+        var student = findByEmail(email);
+        if (student != null) {
+            STUDENTS.remove(student);
+        }
+    }
+}
